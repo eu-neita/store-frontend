e9749dbf72247e7ce18f8ce837a1a310b38e01d1
jeferson-viana <jefersonv28@gmail.com>
2023-03-17 15:41:51 -0300

    Requesito 1 concluído

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import { React, Component } from 'react';
 import './App.css';
+import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  // state = {  }
+  componentDidMount() {
+    getCategories();
+    getProductsFromCategoryAndQuery();
+  }
+
+  render() {
+    return (<div>Hello World!</div>);
+  }
 }
 
 export default App;
