14f5061961b6562d497243593455f3f0ca9fcce5
Nathan de assis Crevelin <neitass.music@gmail.com>
2023-03-20 18:15:39 -0300

    requisito 8 terminado refatorado

--- src/pages/Carrinho.jsx
+++ src/pages/Carrinho.jsx
@@ -20,24 +20,24 @@ class Carrinho extends React.Component {
 
   render() {
     const { arrayCarrinho } = this.state;
-    const mapCart = arrayCarrinho.map((product, i) => (
-      <div key={ i } data-testid="shopping-cart-product-name">
-        <img src={ product.thumbnail } alt={ product.title } />
-        <p>{product.title}</p>
-        <p>{`R$ ${product.price}`}</p>
-        <p data-testid="shopping-cart-product-quantity">
-          1
-        </p>
-      </div>
-    ));
+    console.log(arrayCarrinho);
     return (
       <div>
         <h2>Carrinho</h2>
-        {arrayCarrinho === [] ? (
-          <p data-testid="shopping-cart-empty-message">
-            Seu carrinho está vazio
-          </p>
-        ) : mapCart}
+        {arrayCarrinho === null
+          && <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>}
+        {arrayCarrinho !== null && (
+          arrayCarrinho.map((product, i) => (
+            <div key={ i } data-testid="shopping-cart-product-name">
+              <img src={ product.thumbnail } alt={ product.title } />
+              <p>{product.title}</p>
+              <p>{`R$ ${product.price}`}</p>
+              <p data-testid="shopping-cart-product-quantity">
+                1
+              </p>
+            </div>
+          ))
+        )}
       </div>
     );
   }
