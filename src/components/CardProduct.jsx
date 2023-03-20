daa4799f0fad9a2119d891ae963d9e3848f5766d
jeferson-viana <jefersonv28@gmail.com>
2023-03-20 16:35:06 -0300

    Requisito 7 concluído

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,11 +1,24 @@
 import { Component } from 'react';
 import { PropTypes } from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class CardProduct extends Component {
+  handleClick = (arr) => {
+    window.location.pathname = '/especificacao';
+    localStorage.setItem('product', JSON.stringify(arr));
+  };
+
   render() {
     const { products } = this.props;
     const mapCardsProd = products.map((arr, i) => (
       <div key={ i } data-testid="product">
+        <Link
+          to="/especificacao"
+          data-testid="product-detail-link"
+          onClick={ () => this.handleClick(arr) }
+        >
+          Detalhes
+        </Link>
         <img src={ arr.thumbnail } alt={ arr.title } />
         <p>{arr.title}</p>
         <p>{ `R$ ${arr.price}` }</p>
