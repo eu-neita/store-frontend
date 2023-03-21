a0223b478511524f9ce37a2c5a82e678d4ff77fe
jeferson-viana <jefersonv28@gmail.com>
2023-03-21 18:25:14 -0300

    Requisito 10 concluído

--- src/pages/Carrinho.jsx
+++ src/pages/Carrinho.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 class Carrinho extends React.Component {
   state = {
     arrayCarrinho: [],
-    teste: 1,
   };
 
   componentDidMount() {
@@ -18,29 +17,33 @@ class Carrinho extends React.Component {
     return result;
   }
 
-  handleClick = ({ target }, product) => {
-    if (target.innerText === '-') {
-      product.quanty = product.quanty <= 1 ? 1 : Number(product.quanty) - 1;
-      const func = this.getLocalCart();
-      const filtro = func.filter((elemento) => elemento.title === product.title);
-      const filtro1 = func.filter((elemento) => elemento.title !== product.title);
-      // console.log(filtro);
-      // console.log(filtro1);
-      filtro[0].quanty = product.quanty;
-      localStorage.setItem('carrinho', JSON.stringify([...filtro, ...filtro1]));
+  handleClick = (product, decreaseOrIncrease) => {
+    const { arrayCarrinho: arrayNewCarrinho } = this.state;
+    let position = 0;
+    if (decreaseOrIncrease === '-') {
+      product.quanty = Number(product.quanty) <= 1 ? 1 : Number(product.quanty) - 1;
+      const objectFilter = arrayNewCarrinho.find((elemento, index) => {
+        position = index;
+        return elemento.title === product.title;
+      });
+      arrayNewCarrinho[position].quanty = product.quanty;
+      objectFilter.quanty = product.quanty;
+      localStorage.setItem('carrinho', JSON.stringify(arrayNewCarrinho));
       this.setState({
-        teste: product.quanty,
+        arrayCarrinho: arrayNewCarrinho,
       });
       return;
     }
     product.quanty = Number(product.quanty) + 1;
-    const func = this.getLocalCart();
-    const filtro = func.filter((elemento) => elemento.title === product.title);
-    const filtro1 = func.filter((elemento) => elemento.title !== product.title);
-    filtro[0].quanty = product.quanty;
-    localStorage.setItem('carrinho', JSON.stringify([...filtro, ...filtro1]));
+    const objectFilter = arrayNewCarrinho.find((elemento, index) => {
+      position = index;
+      return elemento.title === product.title;
+    });
+    arrayNewCarrinho[position].quanty = product.quanty;
+    objectFilter.quanty = product.quanty;
+    localStorage.setItem('carrinho', JSON.stringify(arrayNewCarrinho));
     this.setState({
-      teste: product.quanty,
+      arrayCarrinho: arrayNewCarrinho,
     });
   };
 
@@ -54,7 +57,7 @@ class Carrinho extends React.Component {
   };
 
   render() {
-    const { arrayCarrinho, teste } = this.state;
+    const { arrayCarrinho } = this.state;
     return (
       <div>
         <h2>Carrinho</h2>
@@ -67,17 +70,17 @@ class Carrinho extends React.Component {
               <p>{product.title}</p>
               <p>{`R$ ${product.price}`}</p>
               <button
-                onClick={ (event) => this.handleClick(event, product) }
+                onClick={ () => this.handleClick(product, '-') }
                 data-testid="product-decrease-quantity"
                 type="button"
               >
                 -
               </button>
               <p data-testid="shopping-cart-product-quantity">
-                { product.quanty || teste }
+                { product.quanty }
               </p>
               <button
-                onClick={ (event) => this.handleClick(event, product) }
+                onClick={ () => this.handleClick(product, '+') }
                 data-testid="product-increase-quantity"
                 type="button"
               >
