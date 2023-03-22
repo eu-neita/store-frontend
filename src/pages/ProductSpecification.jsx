6bf9cb2ffebc33fb91111cd40b8ede854f74fafa
Nathan de assis Crevelin <neitass.music@gmail.com>
2023-03-22 11:06:17 -0300

    --allow-empty

--- src/pages/ProductSpecification.jsx
+++ src/pages/ProductSpecification.jsx
@@ -6,6 +6,10 @@ class ProductSpecification extends React.Component {
   state = {
     isTrue: false,
     object: {},
+    email: '',
+    text: '',
+    radioValue: 0,
+    emailValidate: false,
   };
 
   async componentDidMount() {
@@ -16,8 +20,27 @@ class ProductSpecification extends React.Component {
     });
   }
 
+  textHandleInputs = ({ target }) => {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   handleSubmit = () => {
-    
+    const { object, email, text, radioValue } = this.state;
+    const mailformat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    const MIN_RADIO_VALUE = 0;
+    if (email.match(mailformat) && MIN_RADIO_VALUE < radioValue) {
+      const objectSaveLocal = [{
+        email,
+        text,
+        rating: radioValue,
+      }];
+      localStorage.setItem(object.id, JSON.stringify(objectSaveLocal));
+      return this.setState({ emailValidate: false });
+    }
+    this.setState({ emailValidate: true });
   };
 
   handleClick = () => {
@@ -27,7 +50,7 @@ class ProductSpecification extends React.Component {
   };
 
   render() {
-    const { isTrue, object } = this.state;
+    const { isTrue, object, email, text, emailValidate } = this.state;
     return (
       <div>
         { !isTrue && <h1>Carregando...</h1>}
@@ -55,34 +78,77 @@ class ProductSpecification extends React.Component {
             <div id="formulario">
               <label>
                 <input
+                  name="email"
                   data-testid="product-detail-email"
                   type="email"
+                  value={ email }
                   placeholder="email"
-                  required
+                  onChange={ this.textHandleInputs }
                 />
               </label>
               <label htmlFor="nota1">
-                <input data-testid="1-rating" id="nota1" type="radio" name="nota" />
+                <input
+                  data-testid="1-rating"
+                  value={ 1 }
+                  id="nota1"
+                  type="radio"
+                  name="radioValue"
+                  onChange={ this.textHandleInputs }
+                />
                 1
               </label>
               <label htmlFor="nota2">
-                <input data-testid="2-rating" type="radio" id="nota2" name="nota" />
+                <input
+                  data-testid="2-rating"
+                  value={ 2 }
+                  type="radio"
+                  id="nota2"
+                  name="radioValue"
+                  onChange={ this.textHandleInputs }
+                />
                 2
               </label>
               <label htmlFor="nota3">
-                <input data-testid="3-rating" id="nota3" type="radio" name="nota" />
+                <input
+                  data-testid="3-rating"
+                  value={ 3 }
+                  id="nota3"
+                  type="radio"
+                  name="radioValue"
+                  onChange={ this.textHandleInputs }
+                />
                 3
               </label>
               <label htmlFor="nota4">
-                <input data-testid="4-rating" id="nota4" type="radio" name="nota" />
+                <input
+                  data-testid="4-rating"
+                  value={ 4 }
+                  id="nota4"
+                  type="radio"
+                  name="radioValue"
+                  onChange={ this.textHandleInputs }
+                />
                 4
               </label>
               <label htmlFor="nota5">
-                <input data-testid="5-rating" id="nota5" type="radio" name="nota" />
+                <input
+                  data-testid="5-rating"
+                  value={ 5 }
+                  id="nota5"
+                  type="radio"
+                  name="radioValue"
+                  onChange={ this.textHandleInputs }
+                />
                 5
               </label>
               <label>
-                <input type="text" data-testid="product-detail-evaluation" />
+                <input
+                  type="text"
+                  name="text"
+                  value={ text }
+                  onChange={ this.textHandleInputs }
+                  data-testid="product-detail-evaluation"
+                />
               </label>
               <br />
               <label>
@@ -93,6 +159,7 @@ class ProductSpecification extends React.Component {
                   value="Enviar"
                 />
               </label>
+              {emailValidate && <p data-testid="error-msg">Campos inválidos</p>}
             </div>
           </div>
         )}
